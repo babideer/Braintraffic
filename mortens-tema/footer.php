4999b593cb96f46d3b8abd1b53d21018f4fcca4c
babideer <55578454+babideer@users.noreply.github.com>
2020-10-04 22:45:26 +0200

    Uploading files

---
+++ mortens-tema/footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package mortens-tema
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<p>&copy; 2020 Amanda Bløndal 
+			<a href="#">Confab Conference</a> 
+			<a href="#">Button Conference</a> 
+			<a href="#">Content Strategy Podcast</a> 
+			<a href="#">Contact Us</a> 
+			<a href="#">Newsletter</a> 
+			<a href="#">Twitter</a> 
+			<a href="#">LinkedIn</a> 
+			</p>
+
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+<!-- Font Awesome-script -->
+<script src="https://kit.fontawesome.com/269c89033b.js" crossorigin="anonymous"></script>
+</body>
+</html>
